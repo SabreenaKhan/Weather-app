06880a8291f1a7dc3b8e50ba6d8a2670b0057ad3
Sabreena Khan <sabreenakhan945@gmail.com>
2026-03-29 12:03:26 +0530

    weather app files commit

---
+++ script.js
@@ -0,0 +1,29 @@
+async function getWeather() {
+  const location = document.getElementById("locationInput").value;
+
+  if (!location) {
+    alert("Please enter a city name");
+    return;
+  }
+
+  const apiKey = "2eaa22d9ef414b9db1260929262903";
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.error) {
+      alert("City not found!");
+      return;
+    }
+
+    document.getElementById("city").innerText = data.location.name + ", " + data.location.country;
+    document.getElementById("temperature").innerText = "Temperature: " + data.current.temp_c + "°C";
+    document.getElementById("condition").innerText = "Condition: " + data.current.condition.text;
+
+  } catch (error) {
+    console.error(error);
+    alert("Error fetching weather data");
+  }
+}
